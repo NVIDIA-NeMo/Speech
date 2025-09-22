046ee11eda9cd8efe4bb9975a9d5454bc5602bb8
Zhengjiang Shao <zshao@nvidia.com>
2025-09-22 20:47:20 +0800

    fix: keep the original callbacks order in CallbackGroup when merging with trainer.callbacks

--- nemo/lightning/callback_group.py
+++ nemo/lightning/callback_group.py
@@ -65,8 +65,8 @@ class CallbackGroup:
                 method = getattr(cb, 'update_config')
                 if callable(method):
                     method(nemo_version=nemo_version, trainer=trainer, **kwargs)
-            # CallbackGroup callback functions should be called first
-            trainer.callbacks.insert(0, cb)
+        # CallbackGroup callback functions should be called first
+        trainer.callbacks = self._callbacks + trainer.callbacks
 
     @property
     def callbacks(self) -> List['BaseCallback']:

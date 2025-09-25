8cf91a97aa62dc3f28b31b91d0529aeaa13dae66
racoiaws <racoiaws@users.noreply.github.com>
2025-09-25 16:10:58 +0000

    Apply isort and black reformatting
    Signed-off-by: racoiaws <racoiaws@users.noreply.github.com>

--- examples/audio/save_augmented.py
+++ examples/audio/save_augmented.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from itertools import islice
 from pathlib import Path
 
@@ -46,58 +60,62 @@ Optional config parameter `num_samples` can be used to limit the number of sampl
 If not specified, the dataloader is used until exhausted.
 """
 
+
 def check_input_cuts(input_cuts_path: Path) -> None:
     assert input_cuts_path.exists(), "input_cuts must exist"
     assert input_cuts_path.suffix == '.jsonl', "input_cuts must be a .jsonl file"
     assert input_cuts_path.parent.exists(), "input_cuts parent directory must exist"
     cuts = lhotse.CutSet.from_file(input_cuts_path)
-    for i,cut in enumerate(cuts):
+    for i, cut in enumerate(cuts):
         assert isinstance(cut, MonoCut), f"{i}th cut is a {type(cut)}, not a MonoCut"
         assert len(cut.recording.sources) == 1, f"{i}th cut has {len(cut.recording.sources)} sources"
         assert cut.recording.sources[0].source is not None, "{i}th cut has no audio source specified"
-        
+
         recording_path = Path(cut.recording.sources[0].source)
         assert not recording_path.is_absolute(), f"{i}th cut's recording source is an absolute path: {recording_path}"
 
         recording_path_full = input_cuts_path.parent / recording_path
         assert recording_path_full.exists(), f"{i}th cut's recording source file does not exist: {recording_path_full}"
 
+
 @hydra.main(config_path="conf", config_name="flow_matching_generative_finetuning.yaml")
 def main(cfg: DictConfig):
-    assert cfg.get("input_cuts", None) is not None, "input_cuts is required, please override (for example, +input_cuts=some_path/cuts.jsonl)"
-    assert cfg.get("output_cuts", None) is not None, "output_cuts is required, please override (for example, +output_cuts=some_path/cuts.augmented.jsonl)"
+    assert (
+        cfg.get("input_cuts", None) is not None
+    ), "input_cuts is required, please override (for example, +input_cuts=some_path/cuts.jsonl)"
+    assert (
+        cfg.get("output_cuts", None) is not None
+    ), "output_cuts is required, please override (for example, +output_cuts=some_path/cuts.augmented.jsonl)"
     num_samples = cfg.get("num_samples", None)
     sample_rate = cfg.model.sample_rate
     keep_directory_structure = cfg.get("keep_directory_structure", False)
 
     input_cuts_path = Path(cfg.input_cuts)
     output_cuts_path = Path(cfg.output_cuts)
-    check_input_cuts(input_cuts_path) # throws an exception if they aren't ok
+    check_input_cuts(input_cuts_path)  # throws an exception if they aren't ok
 
     assert output_cuts_path.parent.exists(), "output_cuts parent directory must exist"
 
     OmegaConf.set_struct(cfg, True)
     OmegaConf.update(cfg, "model.train_ds.cuts_path", str(input_cuts_path), force_add=True)
-    OmegaConf.update(cfg, "model.train_ds.shuffle", False) # ensure deterministic behavior
+    OmegaConf.update(cfg, "model.train_ds.shuffle", False)  # ensure deterministic behavior
     OmegaConf.update(cfg, "model.train_ds.batch_size", 1)
     OmegaConf.update(cfg, "model.train_ds.shard_seed", 0, force_add=True)  # ensure deterministic behavior
     if cfg.model.train_ds.get("sample_rate", None) != sample_rate:
         OmegaConf.update(cfg, "model.train_ds.sample_rate", sample_rate, force_add=True)
 
     dataloader = get_lhotse_dataloader_from_config(
-        OmegaConf.create(cfg.model.train_ds),
-        global_rank=0,
-        world_size=1,
-        dataset=LhotseAudioToTargetDataset()
+        OmegaConf.create(cfg.model.train_ds), global_rank=0, world_size=1, dataset=LhotseAudioToTargetDataset()
     )
-
 
     cuts = lhotse.CutSet.from_file(input_cuts_path)
     if num_samples is None:
         num_samples = len(cuts)
 
     with CutSet.open_writer(output_cuts_path) as writer:
-        for i, (sample, original_cut) in enumerate(tqdm(zip(islice(dataloader, num_samples), cuts), total=num_samples)):
+        for i, (sample, original_cut) in enumerate(
+            tqdm(zip(islice(dataloader, num_samples), cuts), total=num_samples)
+        ):
             # batch_size is 1, so we can access the first element
             dirty = sample['input_signal'][0].numpy()
             clean = sample['target_signal'][0].numpy()
@@ -110,7 +128,7 @@ def main(cfg: DictConfig):
             if keep_directory_structure:
                 # definitely a relative path because we checked for that earlier
                 input_relative_path = Path(original_cut.recording.sources[0].source)
-                
+
                 dirty_path = output_cuts_path.parent / input_relative_path.with_suffix('.dirty.flac')
                 clean_path = output_cuts_path.parent / input_relative_path.with_suffix('.clean.flac')
 
@@ -129,12 +147,21 @@ def main(cfg: DictConfig):
             dirty_recording.sources[0].source = str(dirty_path.relative_to(output_cuts_path.parent))
             clean_recording = Recording.from_file(clean_path)
             clean_recording.sources[0].source = str(clean_path.relative_to(output_cuts_path.parent))
-            
-            cut = MonoCut(id=dirty_recording.id, start=0, channel=0, duration=dirty_recording.duration, recording=dirty_recording)
+
+            cut = MonoCut(
+                id=dirty_recording.id, start=0, channel=0, duration=dirty_recording.duration, recording=dirty_recording
+            )
             cut.target_recording = clean_recording
 
-            for optional_field_name in ('text', 'original_text', 'language',):
-                if hasattr(original_cut, optional_field_name) and getattr(original_cut, optional_field_name) is not None:
+            for optional_field_name in (
+                'text',
+                'original_text',
+                'language',
+            ):
+                if (
+                    hasattr(original_cut, optional_field_name)
+                    and getattr(original_cut, optional_field_name) is not None
+                ):
                     setattr(cut, optional_field_name, getattr(original_cut, optional_field_name))
 
             writer.write(cut)

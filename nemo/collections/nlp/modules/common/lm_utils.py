27a081cfa08b6a13945de88287052fa5f820714f
dimapihtar <dpihtar@gmail.com>
2025-10-09 06:19:48 -0700

    fix style
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- nemo/collections/nlp/modules/common/lm_utils.py
+++ nemo/collections/nlp/modules/common/lm_utils.py
@@ -13,6 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# pylint: skip-file
+# flake8: noqa
+
 import os
 from typing import List, Optional, Union
 

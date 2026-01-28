189a07931cea460becc31abda07caaecbdaaf942
suiyoubi <suiyoubi@users.noreply.github.com>
2026-01-28 14:56:58 +0000

    Apply isort and black reformatting
    Signed-off-by: suiyoubi <suiyoubi@users.noreply.github.com>

--- run/checkpoint_convert.py
+++ run/checkpoint_convert.py
@@ -1,18 +1,20 @@
-from nemo.collections import llm
 import argparse
 
+from nemo.collections import llm
+
+
 def parse_args():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--hf-checkpoint-path", type=str, required=True)
-  parser.add_argument("--nemo-checkpoint-path", type=str, required=True)
-  return parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hf-checkpoint-path", type=str, required=True)
+    parser.add_argument("--nemo-checkpoint-path", type=str, required=True)
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-  args = parse_args()
+    args = parse_args()
 
-
-  llm.import_ckpt(
-      model=llm.DeepSeekModel(llm.DeepSeekV3Config()),
-      source=args.hf_checkpoint_path,
-      output_path=args.nemo_checkpoint_path,
-  )
+    llm.import_ckpt(
+        model=llm.DeepSeekModel(llm.DeepSeekV3Config()),
+        source=args.hf_checkpoint_path,
+        output_path=args.nemo_checkpoint_path,
+    )

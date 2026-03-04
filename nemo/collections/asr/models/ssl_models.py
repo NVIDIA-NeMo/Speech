dc314fdbe27a67d10588bbc6aec6c0697659b47e
He Huang <heh@nvidia.com>
2026-03-04 13:48:15 -0800

    remove nvtx
    Signed-off-by: He Huang <heh@nvidia.com>

--- nemo/collections/asr/models/ssl_models.py
+++ nemo/collections/asr/models/ssl_models.py
@@ -1084,23 +1084,17 @@ class EncDecDenoiseMaskedTokenPredModel(EncDecMaskedTokenPredModel):
             )
             masks = self.pre_encoder.get_current_mask()
         else:
-            with torch.cuda.nvtx.range("quantizer"):
-                _, tokens = self.quantizer(input_signal=processed_signal)
+            _, tokens = self.quantizer(input_signal=processed_signal)
             if apply_mask:
-                with torch.cuda.nvtx.range("mask_processor"):
-                    masked_signal, masks = self.mask_processor(
-                        input_feats=processed_noisy_input_signal, input_lengths=processed_noisy_input_signal_length
-                    )
+                masked_signal, masks = self.mask_processor(
+                    input_feats=processed_noisy_input_signal, input_lengths=processed_noisy_input_signal_length
+                )
             else:
                 masked_signal = processed_noisy_input_signal
                 masks = torch.zeros_like(processed_noisy_input_signal)
-            with torch.cuda.nvtx.range("encoder"):
-                encoded, encoded_len = self.encoder(
-                    audio_signal=masked_signal, length=processed_noisy_input_signal_length
-                )
+            encoded, encoded_len = self.encoder(audio_signal=masked_signal, length=processed_noisy_input_signal_length)
 
-        with torch.cuda.nvtx.range("decoder"):
-            log_probs = self.decoder(encoder_output=encoded)
+        log_probs = self.decoder(encoder_output=encoded)
 
         return log_probs, encoded_len, masks, tokens
 

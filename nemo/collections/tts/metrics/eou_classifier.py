af3b40c9773c7e1728cf20a4788463452e8a1778
Subhankar Ghosh <subhankarg@nvidia.com>
2026-03-16 13:19:50 -0400

    Enhance StrEnum compatibility for Python versions
    Added compatibility for StrEnum in Python versions < 3.11.
    
    Signed-off-by: Subhankar Ghosh <subhankarg@nvidia.com>

--- nemo/collections/tts/metrics/eou_classifier.py
+++ nemo/collections/tts/metrics/eou_classifier.py
@@ -42,6 +42,9 @@ Usage:
 import math
 from dataclasses import dataclass, field
 
+# StrEnum is part of enum in python >= 3.11, for backward compatibility
+# to python < 3.11 we import StrEnum from strenum. Use-case: Huggignface 
+# demo works on python version 3.10
 try:
     from enum import StrEnum
 except ImportError:

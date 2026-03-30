fe526979f32b6919c43d978951e277fd507d0654
stevehuang52 <stevehuang52@users.noreply.github.com>
2026-03-30 19:36:47 +0000

    Apply isort and black reformatting
    Signed-off-by: stevehuang52 <stevehuang52@users.noreply.github.com>

--- nemo/collections/asr/modules/__init__.py
+++ nemo/collections/asr/modules/__init__.py
@@ -20,11 +20,11 @@ from nemo.collections.asr.modules.audio_preprocessing import (  # noqa: F401
     SpectrogramAugmentation,
 )
 from nemo.collections.asr.modules.beam_search_decoder import BeamSearchDecoderWithLM  # noqa: F401
-from nemo.collections.asr.modules.conformer_encoder import (
+from nemo.collections.asr.modules.conformer_encoder import (  # noqa: F401
     ConformerEncoder,
     ConformerEncoderAdapter,
     ConformerMultiLayerFeatureExtractor,
-) # noqa: F401
+)
 from nemo.collections.asr.modules.conv_asr import (  # noqa: F401
     ConvASRDecoder,
     ConvASRDecoderClassification,
@@ -52,7 +52,6 @@ from nemo.collections.asr.modules.ssl_modules import (
     RandomBlockMasking,
     RandomProjectionVectorQuantizer,
 )
-
 
 __all__ = [
     'AudioToMelSpectrogramPreprocessor',

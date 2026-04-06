65d4c86bd91c46c2f5a84884cb3046fa7c3caae9
Jason <jasoli@nvidia.com>
2026-04-06 08:48:21 -0700

    tighten bounds
    Signed-off-by: Jason <jasoli@nvidia.com>

--- tests/functional_tests/L2_TTS_InferEvaluate_EasyMagpietts.sh
+++ tests/functional_tests/L2_TTS_InferEvaluate_EasyMagpietts.sh
@@ -35,7 +35,7 @@ CUBLAS_WORKSPACE_CONFIG=:4096:8 HF_HUB_OFFLINE=1 TORCH_FORCE_NO_WEIGHTS_ONLY_LOA
     --phoneme_tokenizer_path /home/TestData/tts/2603_EasyMagpieTTS/bpe_ipa_tokenizer_2048_en_de_es_fr_hi_it_vi_zh.json \
     --temperature 0.6 \
     --clean_up_disk \
-    --cer_target 0.20 \
-    --ssim_target 0.65 \
+    --cer_target 0.10 \
+    --ssim_target 0.70 \
     --asr_model_name /home/TestData/tts/pretrained_models/parakeet-tdt-1.1b/parakeet-tdt-1.1b.nemo \
     --eou_model_name /home/TestData/tts/pretrained_models/wav2vec2-base-960h

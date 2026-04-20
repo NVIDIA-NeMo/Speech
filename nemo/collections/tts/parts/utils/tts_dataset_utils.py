6ff1720d9334c9bbb20f7cc3d6ad9cb6c4fc41d6
Ryan <rlangman@nvidia.com>
2026-04-20 15:36:37 -0700

    Add import guard
    Signed-off-by: Ryan <rlangman@nvidia.com>

--- nemo/collections/tts/parts/utils/tts_dataset_utils.py
+++ nemo/collections/tts/parts/utils/tts_dataset_utils.py
@@ -27,13 +27,20 @@ import librosa
 import numpy as np
 import torch
 from einops import rearrange
-from nemo_text_processing.text_normalization.normalize import Normalizer
 from scipy import ndimage
 from torch.special import gammaln
 
 from nemo.collections.asr.parts.preprocessing.segment import AudioSegment
 from nemo.collections.audio.parts.utils.transforms import resample
 from nemo.collections.common.parts.utils import mask_sequence_tensor
+
+try:
+    from nemo_text_processing.text_normalization.normalize import Normalizer
+
+    PYNINI_AVAILABLE = True
+except (ImportError, ModuleNotFoundError):
+    Normalizer = None
+    PYNINI_AVAILABLE = False
 
 
 def get_abs_rel_paths(input_path: Path, base_path: Path) -> Tuple[Path, Path]:
@@ -775,7 +782,7 @@ def resample_batch(audio, audio_len, input_sample_rate, output_sample_rate):
     return audio, audio_len
 
 
-def normalize_text_by_pattern(text, pattern, replacement):
+def normalize_text_by_pattern(text: str, pattern: str, replacement: str) -> str:
     """Normalize input text using a regular expression.
 
     This function will search for and replace any string matching the input 'pattern' surrounded by any punctuation.
@@ -811,7 +818,7 @@ class TextProcessor(ABC):
     """Interface for preprocessing text for TTS training, inference, and evaluation"""
 
     @abstractmethod
-    def normalize_text(self, text):
+    def normalize_text(self, text: str) -> str:
         """
         Preprocess text for model training and inference.
             Usually this involves language-specific rules for converting written text to spoken form.
@@ -825,7 +832,7 @@ class TextProcessor(ABC):
         pass
 
     @abstractmethod
-    def process_text_for_wer(self, text):
+    def process_text_for_wer(self, text: str) -> str:
         """
         Preprocess text for calculating word error rate and character error rate.
             This should include conversion of text to lower case, removal of punctuation, normalization,
@@ -843,10 +850,10 @@ class TextProcessor(ABC):
 class DefaultTextProcessor(TextProcessor):
     """Default text processing behavior, if language-specific processing is not yet implemented."""
 
-    def normalize_text(self, text):
+    def normalize_text(self, text: str) -> str:
         return text
 
-    def process_text_for_wer(self, text):
+    def process_text_for_wer(self, text: str) -> str:
         text = text.lower()
         # Replace dash with a single space
         text = text.replace("-", " ")
@@ -868,13 +875,20 @@ class EnglishTextProcessor(TextProcessor):
     but "mr" will not be. This class manually normalizes abbreviations commonly found in public datasets and ASR transcriptions.
     """
 
-    def __init__(self, input_case="cased"):
+    def __init__(self, input_case: str = "cased"):
         super().__init__()
         self.default_processor = DefaultTextProcessor()
-        self.normalizer = Normalizer(lang="en", input_case=input_case)
 
-    def normalize_text(self, text):
-        text = self.normalizer.normalize(text)
+        if not PYNINI_AVAILABLE:
+            logging.warning("`nemo_text_processing` is not installed, will skip default text normalization")
+            self.normalizer = None
+        else:
+            self.normalizer = Normalizer(lang="en", input_case=input_case)
+
+    def normalize_text(self, text: str) -> str:
+        if self.normalizer is not None:
+            text = self.normalizer.normalize(text)
+
         text = normalize_text_by_pattern(text=text, pattern="[mM]r\.?", replacement="mister")
         text = normalize_text_by_pattern(text=text, pattern="[mM]s\.?", replacement="miss")
         text = normalize_text_by_pattern(text=text, pattern="[mM]rs\.?", replacement="missus")
@@ -883,13 +897,13 @@ class EnglishTextProcessor(TextProcessor):
         text = normalize_text_by_pattern(text=text, pattern="[eE]tc\.?", replacement="et cetera")
         return text
 
-    def process_text_for_wer(self, text):
+    def process_text_for_wer(self, text: str) -> str:
         text = self.normalize_text(text)
         text = self.default_processor.process_text_for_wer(text)
         return text
 
 
-def get_text_processor(language):
+def get_text_processor(language: str) -> TextProcessor:
     if language == "en":
         return EnglishTextProcessor()
     else:

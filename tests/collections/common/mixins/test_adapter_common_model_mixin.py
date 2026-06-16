3fbdc9ce7024d03ed036062d70bf126e13bd369b
Charlie Truong <chtruong@nvidia.com>
2026-06-16 14:48:12 -0500

    Remove unused import in test
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- tests/collections/common/mixins/test_adapter_common_model_mixin.py
+++ tests/collections/common/mixins/test_adapter_common_model_mixin.py
@@ -22,7 +22,6 @@ from omegaconf import DictConfig, OmegaConf
 
 from nemo.collections.common.parts.adapter_modules import LinearAdapter
 from nemo.core import ModelPT, NeuralModule
-from nemo.core.classes.common import safe_instantiate
 from nemo.core.classes.mixins import adapter_mixin_strategies, adapter_mixins
 from nemo.core.classes.mixins.adapter_mixins import AdapterModelPTMixin, AdapterModuleMixin
 from nemo.utils import logging, logging_mode
